2e098d2e242a02819af5495ab1c728f2156cbd52
Михаил Карелин <Mikhail625@protonmail.com>
2022-01-17 12:02:23 +0300

    Create MSWin10x64 VS2019 - copy all files new project from folder

---
+++ ConsAppTest_v2/ConsAppTest_v2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsAppTest_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
